f95fd6472c86d27c9b7ee8afc4c149b832aecd2b
Denys Zaporozhchenko <azovpoly@gmail.com>
2022-05-10 10:48:08 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import store from './store';
+import { Route, Routes } from 'react-router-dom';
 import App from './components/app/app';
-import ErrorBoundry from './components/error-boundry';
-import BookStoreService from './services/bookstrore-services';
-import { BookstoreServiceProvider } from './components/bookstore-service-context';
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-import store from './strore';
-
-const bookstoreService = new BookStoreService();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ErrorBoundry>
-        <BookstoreServiceProvider value={bookstoreService}>
-          <Router>
-            <App/>
-
-           {/* { console.log(bookstoreService.getBooks())} */}
-          </Router>
-        </BookstoreServiceProvider>
-      </ErrorBoundry>
+        <App/>
     </Provider>
   </React.StrictMode>
 );
+
+  // <Routes>
+    {/* <Route path='/' element={<Home/>} exact/> */}
+    {/* <Route path='/car' element={<CarDetail/>}/> */}
+  {/* </Routes> */}
